bab8e693e7609ec6df6d9421871fdff46f920a73
Jakob Friedrich <jatfr05@gmail.com>
2025-05-28 18:33:36 +0200

    feat: add basic structure add configs for yarn, biome

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home: React.FC = () => {
+	return (
+		<div>
+			<h1>TUM.ai</h1>
+			<p>This is the homepage of TUM.ai.</p>
+		</div>
+	);
+};
+
+export default Home;
