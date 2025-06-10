7c79d7bc66c49a5cda7a08a3f72a1ccdeb71b71e
Jakob Friedrich <jatfr05@gmail.com>
2025-06-10 12:15:04 +0200

    feat: add footer to website

---
+++ src/components/ui/Section.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { type VariantProps, cva } from "class-variance-authority";
+
+
+const styles = cva("relative p-8 sm:py-16 lg:py-32", {
+  variants: {
+    background: {
+      white: "",
+      inverted: "bg-linear-to-b from-blue-700 to-blue-800 text-white",
+    },
+  },
+  defaultVariants: {
+    background: "white",
+  },
+});
+
+export interface Props extends VariantProps<typeof styles> {
+  children: React.ReactNode;
+  className?: string;
+  backgroundImage?: string;
+}
+
+export default function Section({
+  background,
+  children,
+  backgroundImage,
+  className,
+}: Props) {
+  return (
+    <section className={styles({ background, className: className })}>
+      {backgroundImage && (
+        <img
+          src={backgroundImage}
+          alt="Hero background image"
+          className="absolute -z-10 object-cover"
+        />
+      )}
+      <div className="container mx-auto">{children}</div>
+    </section>
+  );
+}
