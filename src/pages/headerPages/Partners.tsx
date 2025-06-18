ea584c46e9e7d71f3122f7cc393912399947d88c
Jakob Friedrich <jatfr05@gmail.com>
2025-06-18 17:07:00 +0200

    fix: styling on partners page

--- src/pages/headerPages/Partners.tsx
+++ src/pages/headerPages/Partners.tsx
@@ -57,8 +57,6 @@ export default function Partners() {
 
         {/* Content Container */}
         <div className="relative container mx-auto flex min-h-[80vh] max-w-4xl flex-col justify-center p-8 text-white md:p-16">
-          {/* Decorative Elements */}
-          <div className="absolute top-1/2 left-0 h-32 w-1 -translate-y-1/2 bg-linear-to-b from-blue-500 to-purple-500 opacity-50" />
 
           {/* Main Content */}
           <div className="space-y-6">
