713052efad767987de58fade4b3d4299a9922340
Jakob Friedrich <jatfr05@gmail.com>
2025-09-04 20:47:13 +0200

    fix: remove jump in seamless scroll - remove duplicate picture

--- src/components/home/ScrollSection.tsx
+++ src/components/home/ScrollSection.tsx
@@ -7,35 +7,31 @@ export function ScrollSection() {
       id: 2,
       src: "/assets/home_img2.png",
     },
+    { id: 3, src: "public/assets/e-lab/e-phases/ideation.jpeg" },
     {
-      id: 3,
-      src: "public/assets/martin_talk.jpg",
-    },
-    { id: 4, src: "public/assets/e-lab/e-phases/ideation.jpeg" },
-    {
-      id: 5,
+      id: 4,
       src: "public/assets/innovation/med_ai.jpg",
     },
-    { id: 6, src: "/assets/home_img5.jpeg" },
-    { id: 7, src: "/assets/home_img4.jpg" },
+    { id: 5, src: "/assets/home_img5.jpeg" },
+    { id: 6, src: "/assets/home_img4.jpg" },
     {
-      id: 8,
+      id: 7,
       src: "public/assets/innovation/robotics_discussion.png",
     },
-    { id: 9, src: "public/assets/innovation/accelerated_computing.png" },
+    { id: 8, src: "public/assets/innovation/accelerated_computing.png" },
   ];
 
   const doubledData = [...data, ...data]; // duplicate for seamless scroll
 
   return (
     <div className="overflow-hidden w-full my-10">
-      <div className="flex animate-scroll-left">
+      <div className="flex flex-nowrap animate-scroll-left">
         {doubledData.map((imgObj, idx) => (
           <img
             key={idx}
             src={imgObj.src}
             alt=""
-            className="h-80 w-auto flex-shrink-0 mx-4"
+            className="h-48 sm:h-64 lg:h-80 w-auto flex-shrink-0 mx-4"
           />
         ))}
       </div>
