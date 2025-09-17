2168a83f1ae83303e66516d4b4dae0e20c5793b7
Sayf2004 <ketatasayf4@gmail.com>
2025-09-17 22:11:19 +0100

    fix: background and sub-title for community

--- src/components/apply/shared/MemberStories.tsx
+++ src/components/apply/shared/MemberStories.tsx
@@ -10,32 +10,33 @@ interface MemberStoriesProps {
 }
 export default function MemberStories({ stories }: MemberStoriesProps) {
   return (
-    <section className="container mx-auto max-w-5xl px-4 py-16">
-      <h3 className="text-title text-white font-bold mb-4 ">
-        Member{" "}
-        <span className="bg-gradient-to-r from-[#891FDB] to-[#E0189A] text-transparent bg-clip-text">
-          Stories
-        </span>
-      </h3>
-      <div className="grid gap-8 md:grid-cols-2">
-        {stories.map((story) => (
-          <div
-            key={story.name}
-            className="rounded-xl  p-6 shadow-lg flex flex-col items-center bg-white"
-          >
-            <img
-              src={story.image}
-              alt={story.name}
-              className="mb-4 h-32 w-32 rounded-full object-cover"
-            />
-            <h3 className="text-subtitle font-bold text-purple-800">
-              {story.name}
-            </h3>
-            <p className="text-subtext text-gray-600 mb-2">{story.role}</p>
-            <p className="text-base text-gray-800 text-center">{story.story}</p>
-          </div>
-        ))}
-      </div>
+    <section id="memberStories" className="bg-white">
+      <section className="container mx-auto max-w-5xl px-4 py-16">
+        <h3 className="text-title font-bold mb-4 text-purple-600">
+          Member Stories
+        </h3>
+        <div className="grid gap-8 md:grid-cols-2">
+          {stories.map((story) => (
+            <div
+              key={story.name}
+              className="rounded-xl p-6 shadow-lg flex flex-col items-center bg-white"
+            >
+              <img
+                src={story.image}
+                alt={story.name}
+                className="mb-4 h-32 w-32 rounded-full object-cover"
+              />
+              <h3 className="text-subtitle font-bold text-purple-800">
+                {story.name}
+              </h3>
+              <p className="text-subtext text-gray-600 mb-2">{story.role}</p>
+              <p className="text-base text-gray-800 text-center">
+                {story.story}
+              </p>
+            </div>
+          ))}
+        </div>
+      </section>
     </section>
   );
 }
