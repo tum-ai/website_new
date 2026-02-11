6de9a91dba2aabb2e531adb45dd5731c414b4b75
Zexin Gong <zexin.gong@tum.de>
2026-02-11 18:11:46 +0100

    use svg logo

--- src/pages/headerPages/e-lab/hero.tsx
+++ src/pages/headerPages/e-lab/hero.tsx
@@ -38,8 +38,8 @@ export const Hero = () => {
                 <div className="flex justify-center">
                   <div className="h-28 md:h-40 flex items-center">
                     <img
-                      // src="assets/e-lab/E-Lab5Logo.svg"
-                      src="/assets/e-lab/E-Lab5Logo.png"
+                      src="assets/e-lab/E-Lab5Logo.svg"
+                      // src="/assets/e-lab/E-Lab5Logo.png"
                       alt="E-LAB 5.0"
                       width={400}
                       height={160}
